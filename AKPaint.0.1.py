02525ad9899280c3e1ab1ee9692745b8d6b2c861
andrewklajman <andrew.klajman@gmail.com>
2018-01-15 09:20:43 +1100

    Add files via upload

--- AKPaint.0.1.py
+++ AKPaint.0.1.py
@@ -3,25 +3,53 @@ import pygame
 RES = (400,400)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+RED = (255,0,0)
+
+SML = 1
+MED = 5
+LRG = 10
+mPos = 0
+mBtn = 0
+cur_size = SML
 
 pygame.init()
-gd = pygame.display.set_mode(RES)
-gd.fill(WHITE)
 clock = pygame.time.Clock()
+
+
+
+mainSurf = pygame.display.set_mode(RES)
+paintSurf = pygame.Surface((RES[0],RES[0] - 25))
+toolSurf = pygame.Surface((RES[0],25))
+paintSurf.fill(WHITE)
+toolSurf.fill(RED)
+
+mainSurf.blit(toolSurf,(0,0))
+mainSurf.blit(paintSurf,(0,25))
+              
+
 
 def checkQuit():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    
 
-while True:
-    checkQuit()
+def draw_pen(size, color):
+    if size == 1:
+        paintSurf.set_at(mPos, color)
+    else:
+        pygame.draw.circle(paintSurf,color,mPos,size)
+
+def initVariables():
+    global mPos, mBtn
     mPos = pygame.mouse.get_pos()
     mBtn = (pygame.mouse.get_pressed()[0] == 1)
 
+while True:
+    checkQuit()
+    initVariables()
+
     if mBtn:
-        pygame.draw.circle(gd,BLACK,mPos,5)
+        draw_pen(SML, BLACK)
     
     pygame.display.update()
             
